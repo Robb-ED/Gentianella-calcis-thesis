44bd331ea877470b6d33686c54cdae69d7c2dc63
Robb <135808229+Robb-ED@users.noreply.github.com>
2024-01-12 14:51:42 +1300

    Added Stacks and TrimGalore scripts via upload
    These were used for data processing in Chapter 3 of my thesis

---
+++ Chapter 3/trim_galore_script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+##############################################################################
+# Scripts for processing tara iti and Australian fairy tern nova-seq samples
+# recieved 12th April 2021.
+## NOTE: All fairy tern samples have a double peak in their fastqc GC curves.
+##############################################################################
+
+# First began with 2 colour chemistry specific trimmming.
+raw=~/Gentianella/whole_data/SRA_Sequence/
+trim_out=~/Gentianella/whole_data/SRA_Sequence/trimmed_100BP/
+
+for samp in ${raw}*1.fastq.gz
+do
+base=$(basename ${samp} .1.fastq.gz)
+echo "Running Trim_galore for ${base}..."
+trim_galore --paired \
+    --cores 8 \
+    --length 100 \
+    --output_dir ${trim_out}/ \
+    --no_report_file \
+    ${raw}${base}.1.fastq.gz \
+    ${raw}${base}.2.fastq.gz
+done
---
+++ Chapter 3/whole_pipeline_M1-12_m369.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Absolute path to the stacks 2 protocol directory
+work=~/Veronica/mt_white/island_test/samples/trimmomatic
+
+# Parameter end points
+ENDM=12
+ENDi=9
+
+# Loop over the M and m values
+# Iterates over every value between 1-12 for M and 3, 6, 9 for m
+for M in $(seq 1 $ENDM)
+do
+    for i in $(seq 3 3 $ENDi)
+    do
+    # This creates a new output directory per Stacks run
+    out=$work/denovo.M${M}.m${i}
+    mkdir $out
+    # Move into the new directory
+    cd $out
+        # Stacks command
+        denovo_map.pl --samples $work --popmap $work/scripts/popmap.tsv \
+            --out-path $out \
+            --paired \
+            -M $M \
+            -n $M \
+            -T 20 \
+            --min-samples-per-pop 0.8 \
+            -X "ustacks: --force-diff-len" \
+            -X "ustacks: -m ${i}"
+
+    done
+done
