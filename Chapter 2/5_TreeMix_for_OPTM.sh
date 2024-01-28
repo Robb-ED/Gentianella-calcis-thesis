fa37a98fd941465e94e84640bd5921b4eb104f31
Robb <135808229+Robb-ED@users.noreply.github.com>
2024-01-29 11:21:11 +1300

    Added treemix shell script

---
+++ Chapter 2/5_TreeMix_for_OPTM.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for m in $(seq 1 10) 
+do
+    for i in $(seq 1 10) 
+    do
+        # Generate random seed
+        s=$RANDOM
+        # Generate bootstrapped input file with ~80% of the SNP loci
+        gunzip -c treemix_input.gz | awk 'BEGIN {srand()} { if (NR==1) {print $0} else if (rand() <= .8) print $0}' | gzip > treemix${i}.${m}.treemix.gz
+        # Run treemix on bootstrapped input file
+        treemix -i treemix${i}.${m}.treemix.gz -o out${i}.${m} -noRoot -m ${m} -seed ${s} &
+    done &
+    done
+    
